b24af9bcd56a2f3b71a5690e0abacd2dbac5638d
Sharoon Srivastava <srivas15@purdue.edu>
2016-03-29 21:58:04 -0400

    first version

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-myText = "Hello i am trying to test this stupid code"
+import sys
+from bs4 import BeautifulSoup
+import requests
+import urllib2
 
+#myText = "Hello i am trying to test this stupid code"
+
+if(len(sys.argv) != 2):
+	print "Please check the argument again. For more help visit : https://github.com/srivas15/thesaurus"
+file = open(str(sys.argv[1]), 'r')
+myText = file.read()
 print myText
 
 words = myText.split()
@@ -9,6 +18,31 @@ print words
 urlStart = 'http://thesaurus.com/browse/'
 urlEnd = '?s=t'
 
+finalText = ""
+
+dontReplace = ['what', 'why', 'who', 'when', 'where', 'was']
+
 for word in words:
-	url = urlStart+word+urlEnd
-	print url
+	word = word.replace(".", "")
+	word = word.replace("'", "")
+	word = word.replace(",", "")
+	if(len(word) < 3 or word in dontReplace):
+		thesaurus = word
+	else:
+		url = urlStart+word+urlEnd
+		print url
+		
+		var = requests.get(url)
+		html = urllib2.urlopen(var.url).read()
+		soup = BeautifulSoup(html, "html.parser")
+		
+		content = (soup.find("div", {"class" : "relevancy-list"})).find("ul")
+		#print content
+		thesaurus = (content.find("li")).getText()
+		thesaurus = thesaurus[:-4]
+		#print thesaurus
+		#break
+	finalText = finalText + " " + thesaurus
+
+finalText = finalText[1:]
+print finalText
